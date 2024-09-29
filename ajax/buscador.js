c222a5e9dee53a98d8e26870c442b02d89eb8806
schafickMol <141662359+schafickMol@users.noreply.github.com>
2024-09-29 14:28:07 -0600

    Add files via upload

---
+++ ajax/buscador.js
@@ -0,0 +1,11 @@
+function buscar() {
+    let texto = $('#buscador').val();
+    $.ajax({
+      url: 'buscar.php',
+      type: 'POST',
+      data: { texto: texto },
+      success: function (res) {
+        $('#resultado').html(res);
+      },
+    });
+  }
